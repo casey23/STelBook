38f42d5f3747ea24ae93534086072abeb9d6bfda
chen_bing <1479076807@qq.com>
2016-07-10 17:54:17 +0800

    完成主界面(登录界面)，并添加点击事件监听

--- app/src/main/java/com/jay/stelbook/MainActivity.java
+++ app/src/main/java/com/jay/stelbook/MainActivity.java
@@ -2,18 +2,52 @@ package com.jay.stelbook;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.style.ForegroundColorSpan;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements View.OnClickListener {
+
+    private EditText mEmil;
+    private EditText mPsw;
+    private Button mLogin;
+    private TextView mRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//        ACProgressFlower dialog = new ACProgressFlower.Builder(this)
-//                .direction(ACProgressConstant.DIRECT_CLOCKWISE)
-//                .themeColor(Color.WHITE)
-//                .text("Title is here").textColor(0xffffff).textMarginTop(5)
-//                .fadeColor(Color.DKGRAY).build();
-//        dialog.show();
+        initView();
+    }
+
+    /**
+     * 初始化所有控件并添加点击事件
+     */
+    private void initView() {
+        mEmil = (EditText) findViewById(R.id.emil);
+        mPsw = (EditText) findViewById(R.id.password);
+        mLogin = (Button) findViewById(R.id.login);
+        mRegister = (TextView) findViewById(R.id.register);
+        mLogin.setOnClickListener(this);
+        mRegister.setOnClickListener(this);
+        //设置注册文本
+        SpannableString spannableString = new SpannableString("没有账号?点我注册");
+        spannableString.setSpan(new ForegroundColorSpan(0xffa2a2a2),0,5, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+        spannableString.setSpan(new ForegroundColorSpan(0xff5856d6),5,9, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+        mRegister.setText(spannableString);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.login:
+                break;
+            case R.id.register:
+                break;
+        }
     }
 }
