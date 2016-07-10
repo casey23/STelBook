74a13ef48e0c0e6c9da85d7408e22302ac8cdd3e
chen_bing <1479076807@qq.com>
2016-07-10 09:37:27 +0800

    添加自定义等待对话框

--- app/src/main/java/com/jay/stelbook/MainActivity.java
+++ app/src/main/java/com/jay/stelbook/MainActivity.java
@@ -1,13 +1,19 @@
 package com.jay.stelbook;
 
+import android.app.Activity;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+//        ACProgressFlower dialog = new ACProgressFlower.Builder(this)
+//                .direction(ACProgressConstant.DIRECT_CLOCKWISE)
+//                .themeColor(Color.WHITE)
+//                .text("Title is here").textColor(0xffffff).textMarginTop(5)
+//                .fadeColor(Color.DKGRAY).build();
+//        dialog.show();
     }
 }
